9045ecdad8100b82b1ccee28bad1ab0867acd9b8
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-22 22:00:11 +0530

    Update discord_spam.py

--- discord_spam.py
+++ discord_spam.py
@@ -4,7 +4,7 @@ keyboardd = keyboard.Controller()
 till = int(input("No of times: "))
 text = input("text: ")
 time.sleep(10)
-while till >= 0:  # LOOP DE LOOP
+while till >= 1:  # LOOP DE LOOP
     keyboardd.type(str(text))
     keyboardd.press(keyboard.Key.enter)
     keyboardd.release(keyboard.Key.enter)
