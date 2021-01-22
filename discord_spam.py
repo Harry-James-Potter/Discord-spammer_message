718e4434586c616dce3a40b57a90ba70273b62c8
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-01-22 11:27:54 -0500

    Add files via upload

---
+++ discord_spam.py
@@ -0,0 +1,12 @@
+import time
+from pynput import keyboard
+keyboardd = keyboard.Controller()
+till = int(input("No of times: "))
+text = input("text: ")
+time.sleep(10)
+while till >= 0:  # LOOP DE LOOP
+    keyboardd.type(str(text))
+    keyboardd.press(keyboard.Key.enter)
+    keyboardd.release(keyboard.Key.enter)
+    time.sleep(1.5)  # ADJUST ACCORDINGLY
+    till -= 1
