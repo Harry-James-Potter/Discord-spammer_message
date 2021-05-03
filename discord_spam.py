bfdd9e77e6d9a6ddcef509f2f2ab306b1dedd83c
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-05-03 15:26:17 +0530

    Update discord_spam.py

--- discord_spam.py
+++ discord_spam.py
@@ -1,12 +1,14 @@
 import time
 from pynput import keyboard
+
 keyboardd = keyboard.Controller()
 till = int(input("No of times: "))
 text = input("text: ")
-time.sleep(10)
-while till >= 1:  # LOOP DE LOOP
+time.sleep(10)  #  TIME TILL GO TO APP  #
+
+while till >= 1:  #  LOOP DE LOOP  #
     keyboardd.type(str(text))
     keyboardd.press(keyboard.Key.enter)
     keyboardd.release(keyboard.Key.enter)
-    time.sleep(1.5)  # ADJUST ACCORDINGLY
+    time.sleep(1.5)  # ADJUST ACCORDINGLY  #
     till -= 1
